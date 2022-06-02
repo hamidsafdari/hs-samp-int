29508140e04cad2f1f43228dff1512412305094d
Hamid Safdari <hamidsafdari22@gmail.com>
2022-06-02 06:12:22 +0430

    feat: file copy via spring integration

---
+++ src/main/kotlin/af/samp/integ/conf/IntegrationConfig.kt
@@ -0,0 +1,52 @@
+package af.samp.integ.conf
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.integration.annotation.InboundChannelAdapter
+import org.springframework.integration.annotation.Poller
+import org.springframework.integration.annotation.ServiceActivator
+import org.springframework.integration.channel.DirectChannel
+import org.springframework.integration.config.EnableIntegration
+import org.springframework.integration.file.FileReadingMessageSource
+import org.springframework.integration.file.FileWritingMessageHandler
+import org.springframework.integration.file.filters.SimplePatternFileListFilter
+import org.springframework.integration.file.support.FileExistsMode
+import java.io.File
+
+@Configuration
+@EnableIntegration
+class IntegrationConfig {
+  init {
+    val inputDir = File(INPUT_DIR)
+    if (!inputDir.exists()) {
+      inputDir.mkdirs()
+    }
+    val outputDir = File(OUTPUT_DIR)
+    if (!outputDir.exists()) {
+      outputDir.mkdirs()
+    }
+  }
+
+  @Bean
+  fun fileChannel() = DirectChannel()
+
+  @Bean
+  @InboundChannelAdapter(value = "fileChannel", poller = [Poller(fixedDelay = "1000")])
+  fun fileReadingMessageSource() = FileReadingMessageSource().apply {
+    setDirectory(File(INPUT_DIR))
+    setFilter(SimplePatternFileListFilter(FILE_PATTERN))
+  }
+
+  @Bean
+  @ServiceActivator(inputChannel = "fileChannel")
+  fun fileWritingMessageHandler() = FileWritingMessageHandler(File(OUTPUT_DIR)).apply {
+    setFileExistsMode(FileExistsMode.REPLACE)
+    setExpectReply(false)
+  }
+
+  companion object {
+    private const val INPUT_DIR = "the_source_dir"
+    private const val OUTPUT_DIR = "the_dest_dir"
+    private const val FILE_PATTERN = "*.jpg"
+  }
+}
