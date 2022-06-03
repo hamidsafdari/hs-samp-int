b703a41cb5f7a2e3d3ffa96f146b307f83516248
Hamid Safdari <hamidsafdari22@gmail.com>
2022-06-03 17:25:28 +0430

    refactor: rewrite using IntegrationFlow

--- src/main/kotlin/af/samp/integ/conf/IntegrationConfig.kt
+++ src/main/kotlin/af/samp/integ/conf/IntegrationConfig.kt
@@ -1,40 +1,10 @@
 package af.samp.integ.conf
 
-import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.integration.annotation.IntegrationComponentScan
-import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.config.EnableIntegration
-import org.springframework.integration.dsl.MessageChannels
-import org.springframework.messaging.Message
-import org.springframework.messaging.MessageChannel
-import org.springframework.messaging.handler.annotation.Payload
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
 @Configuration
 @EnableIntegration
 @IntegrationComponentScan("af.samp.integ.int")
-class IntegrationConfig {
-  private val logger = LoggerFactory.getLogger(IntegrationConfig::class.java)
-  private val executor = ThreadPoolTaskExecutor().apply {
-    corePoolSize = 16
-    maxPoolSize = 16
-    setThreadNamePrefix("name-t-")
-    initialize()
-  }
-
-  @Bean
-  fun nameRequestChannel(): MessageChannel = MessageChannels.publishSubscribe(executor)
-    .wireTap(loggerChannel())
-    .get()
-
-  @Bean
-  fun loggerChannel(): MessageChannel = MessageChannels.executor(executor).get()
-
-  @ServiceActivator(inputChannel = "loggerChannel")
-  fun channelLogger(@Payload message: Message<*>) {
-    Thread.sleep(150)
-    logger.debug("logging: {}", message)
-  }
-}
+class IntegrationConfig
--- src/main/kotlin/af/samp/integ/service/SimpleService.kt
+++ src/main/kotlin/af/samp/integ/service/SimpleService.kt
@@ -1,23 +1,45 @@
 package af.samp.integ.service
 
-import kotlinx.coroutines.DEBUG_PROPERTY_NAME
+import af.samp.integ.int.SimpleGateway
 import org.slf4j.LoggerFactory
 import org.springframework.integration.annotation.ServiceActivator
+import org.springframework.integration.dsl.IntegrationFlowAdapter
+import org.springframework.integration.dsl.IntegrationFlowBuilder
+import org.springframework.integration.dsl.MessageChannels
+import org.springframework.messaging.Message
 import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.stereotype.Service
-import kotlin.math.log
 import kotlin.random.Random
 
 @Service
-class SimpleService {
+class SimpleService : IntegrationFlowAdapter() {
   private val logger = LoggerFactory.getLogger(SimpleService::class.java)
   private val names = arrayOf("name1", "name2", "name3")
   private val random = Random(System.currentTimeMillis())
+  private val executor = ThreadPoolTaskExecutor().apply {
+    corePoolSize = 16
+    maxPoolSize = 16
+    setThreadNamePrefix("name-t-")
+    initialize()
+  }
+
+  override fun buildFlow(): IntegrationFlowBuilder {
+    return from(SimpleGateway::class.java)
+      .channel(MessageChannels.publishSubscribe("nameRequestChannel", executor).datatype(String::class.java))
+      .wireTap(MessageChannels.publishSubscribe("loggerChannel", executor).get())
+  }
 
   @ServiceActivator(inputChannel = "nameRequestChannel")
   fun getRandomName(@Payload name: String): String {
     logger.debug("service")
     Thread.sleep(random.nextInt(200).toLong())
-    return "${System.currentTimeMillis()}| request: $name | reply: ${names.get(random.nextInt(3))}"
+    return "${System.currentTimeMillis()}| request: $name | reply: ${names[random.nextInt(3)]}"
+  }
+
+  @ServiceActivator(inputChannel = "loggerChannel")
+  fun channelLogger(@Payload message: Message<*>) {
+    Thread.sleep(150)
+    logger.debug("logging: {}", message)
   }
 }
