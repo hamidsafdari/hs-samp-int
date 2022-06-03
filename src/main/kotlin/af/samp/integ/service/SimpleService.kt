2d09908f4545c11b04e12a3242f54a7c134df30a
Hamid Safdari <hamidsafdari22@gmail.com>
2022-06-03 22:30:45 +0430

    feat: router

--- src/main/kotlin/af/samp/integ/service/SimpleService.kt
+++ src/main/kotlin/af/samp/integ/service/SimpleService.kt
@@ -1,6 +1,7 @@
 package af.samp.integ.service
 
 import af.samp.integ.int.SimpleGateway
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.dsl.IntegrationFlowAdapter
@@ -12,11 +13,18 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.stereotype.Service
 import kotlin.random.Random
 
+data class GeneratedName(
+  val request: String,
+  val reply: String,
+  val timestamp: Long
+)
+
 @Service
 class SimpleService : IntegrationFlowAdapter() {
   private val logger = LoggerFactory.getLogger(SimpleService::class.java)
   private val names = arrayOf("name1", "name2", "name3")
   private val random = Random(System.currentTimeMillis())
+  private val mapper = ObjectMapper()
   private val executor = ThreadPoolTaskExecutor().apply {
     corePoolSize = 16
     maxPoolSize = 16
@@ -26,20 +34,30 @@ class SimpleService : IntegrationFlowAdapter() {
 
   override fun buildFlow(): IntegrationFlowBuilder {
     return from(SimpleGateway::class.java)
-      .channel(MessageChannels.publishSubscribe("nameRequestChannel", executor).datatype(String::class.java))
       .wireTap(MessageChannels.publishSubscribe("loggerChannel", executor).get())
+      .channel(MessageChannels.publishSubscribe("nameRequestChannel", executor).datatype(String::class.java))
+      .routeToRecipients { r ->
+        r.applySequence(true)
+          .recipient(MessageChannels.publishSubscribe("jsonChannel", executor).get())
+      }
   }
 
-  @ServiceActivator(inputChannel = "nameRequestChannel")
-  fun getRandomName(@Payload name: String): String {
-    logger.debug("service")
+  @ServiceActivator(inputChannel = "nameRequestChannel", outputChannel = "jsonChannel")
+  fun getRandomName(@Payload name: String): GeneratedName {
+    logger.debug("getRandomName: {}", name)
     Thread.sleep(random.nextInt(200).toLong())
-    return "${System.currentTimeMillis()}| request: $name | reply: ${names[random.nextInt(3)]}"
+    return GeneratedName(name, names[random.nextInt(3)], System.currentTimeMillis())
+  }
+
+  @ServiceActivator(inputChannel = "jsonChannel")
+  fun convertToJson(@Payload generatedName: GeneratedName): String {
+    logger.debug("convertToJson: {}", generatedName)
+    return mapper.writeValueAsString(generatedName)
   }
 
   @ServiceActivator(inputChannel = "loggerChannel")
   fun channelLogger(@Payload message: Message<*>) {
-    Thread.sleep(150)
     logger.debug("logging: {}", message)
+    Thread.sleep(150)
   }
 }
